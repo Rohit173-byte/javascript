386442082fb028445db5408eadebc8adbf2d5707
Rohit173-byte <chamolirohit42@gmail.com>
2024-07-31 18:27:08 +0000

    convertor and  operator

--- convertor.js
+++ convertor.js
@@ -1,6 +1,6 @@
-console.log("rohit");
-let score="33abc";
-console.log(typeof (score));
+
+let score="abc";
+/*console.log(typeof (score));
 let typeconvertor = Number(score);
 console.log(typeof(typeconvertor));
 console.log(typeconvertor);
@@ -8,3 +8,12 @@ let name =null;
 console.log(typeof (name));
 let main = undefined;
 console.log(main);
+let convertor = Boolean(name);*/
+let convertor1 = Boolean(score);
+console.log(typeof(convertor1));
+console.log( "the value of cinvertor1 is =",convertor1);
+// empty string => false, 0 => false
+// name => true , "rohit" => true
+let name = undefined;
+let convertornumber = Number(name);
+console.log(convertornumber);
---
+++ operation.js
@@ -0,0 +1,9 @@
+console.log(2+3);
+console.log("2"+3+3);
+console.log(2+3+"5");
+console.log(2+"3"+3);
+console.log(+true); // 1
+console.log(+"");// 0
+console.log(-"rohit");// nan
+console.log(+"rohit");// nan
+console.log(-rohit); //error 
