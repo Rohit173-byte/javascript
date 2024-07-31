abe0dac10817154f2c5a1afb591cd27171e8fd80
Rohit173-byte <chamolirohit42@gmail.com>
2024-07-31 18:39:09 +0000

    operator

--- operation.js
+++ operation.js
@@ -1,3 +1,4 @@
+ /*
 console.log(2+3);
 console.log("2"+3+3);
 console.log(2+3+"5");
@@ -6,4 +7,13 @@ console.log(+true); // 1
 console.log(+"");// 0
 console.log(-"rohit");// nan
 console.log(+"rohit");// nan
-console.log(-rohit); //error 
+console.log(-rohit); //error  
+                  */
+let num = 5;
+let prefix = ++num;
+console.log(num);
+console.log(prefix);  //  the increment operator increments and returns the value after incrementing.
+ let num1 =7;
+ let postfix = num1 ++
+ console.log(num1);
+console.log(postfix);  //the increment operator increments and returns the value before incrementing.
