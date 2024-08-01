3ce87ac8777d3deda0a77f4baf762416957ae230
Rohit173-byte <chamolirohit42@gmail.com>
2024-08-01 15:44:23 +0000

    dev

---
+++ number_maths.js
@@ -0,0 +1,22 @@
+/*
+
+const first = 100;
+console.log(first);
+const second = new Number(100000.897);
+console.log(second);
+console.log(second.toFixed(3));
+console.log(second.toPrecision(2));
+console.log(second.toLocaleString('en-IN'));
+const third = String(second);
+console.log(typeof(second)); */
+
+/////////// maths ////////////////
+
+
+console.log(Math);
+console.log(Math.abs(-4.67));  // -ve -> +ve & +ve -> +ve
+console.log(Math.floor(2.4));  // smaller value
+console.log(Math.ceil(2.1));  // greater value
+console.log(Math.round(3.8)); // round off
+console.log(Math.min(2,4,6,7,8,9));
+console.log(Math.random()); // value randomally comes between 0 and 1 
