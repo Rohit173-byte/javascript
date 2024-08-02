ae78da4f38494c39dcf0956b77a5421b9b255db4
Rohit173-byte <chamolirohit42@gmail.com>
2024-08-02 08:52:14 +0000

    array

---
+++ basic2/array.js
@@ -0,0 +1,31 @@
+ //const myarr = [2,4,5,6];
+//console.log(myarr);
+/*
+const newarray = new Array(2,5,7,8,9,5,4,2);
+
+newarray.push(56)  // last 
+newarray.unshift(90) // first
+newarray.pop() // last
+newarray.shift() // first
+console.log(newarray.includes(90));  // return boolean type
+console.log(newarray.indexOf(90));  
+console.log(newarray.join());
+
+console.log(newarray);   */
+
+const mna = [2,4,6,7];
+console.log("A",mna);
+
+console.log("B",mna.slice(1,3));
+console.log(mna);                   // don't manupulate original array and last indx don't taken
+ 
+console.log("C",mna.splice(1,3));         
+console.log(mna);                    /// original array manupulation  and last index only taken 
+
+
+
+
+
+
+
+
