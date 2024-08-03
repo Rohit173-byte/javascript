38f89ea01a617b4b95c2323ed151106a61dea3fc
Rohit173-byte <chamolirohit42@gmail.com>
2024-08-03 09:19:01 +0000

    object

--- basic2/object.js
+++ basic2/object.js
@@ -1,17 +1,17 @@
-// const mysym = Symbol("key1");
+const mysym = Symbol("key1");
 
-// const obj = {
-//     name : {
-//         first_name : "rohit",
-//         last_name : "chamoli",
-//         email : "rohit@gmail.com"
-//     },
-//     age : 22,
-//     LOggedIn : true ,
-//     [mysym] : "mykey1",
-//     college : "gehu",
+const obj = {
+    name : {
+        first_name : "rohit",
+        last_name : "chamoli",
+        email : "rohit@gmail.com"
+    },
+    age : 22,
+    LOggedIn : true ,
+    [mysym] : "mykey1",
+    college : "gehu",
     
-// }
+}
 // console.log(obj);
 // console.log(obj.name);
 
@@ -27,19 +27,44 @@
 // console.log(obj.greeting());
 
 
-const obj1 = {
-    a:1,
-    b:2,
-}
-const obj2 = {
-    c:3,
-    d:4,
-}
-const obj3 =  Object.assign({},obj1,obj2);
-console.log(obj3);
+// const obj1 = {
+//     a:1,
+//     b:2,
+// }
+// const obj2 = {
+//     c:3,
+//     d:4,
+// }
+// const obj3 =  Object.assign({},obj1,obj2);
+// console.log(obj3);
  
 
-const obj4  = {...obj1,...obj2}
-console.log(obj4);
+// const obj4  = {...obj1,...obj2}
+// console.log(obj4);
+
+
+// const users = [ 
+//     user1= {
+//         id : 123 ,
+//         clg : "gehu"
+//     },
+//     user2 = {
+//         id :456,
+//         clg :"geu"
+//     }
+// ] ;
+// // console.log(users[0].clg);
+// // console.log(users[1].clg);
+
+// // console.log(Object.keys(obj1));  // convert into array
+// // console.log(Object.keys(obj.name));
+// // console.log(Object.values(obj));
+// console.log(Object.values(obj.name));
+console.log(Object.entries(obj.name));
+
+console.log(obj.hasOwnProperty("name"));
+console.log(obj.name.hasOwnProperty("first_name"));
+
+
 
 
