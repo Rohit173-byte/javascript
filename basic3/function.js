f8619431c598f606fae53077d3b9834a02fa727a
Rohit173-byte <chamolirohit42@gmail.com>
2024-08-04 08:02:55 +0000

    function

---
+++ basic3/function.js
@@ -0,0 +1,19 @@
+// function sum(num1, num2){
+// console.log(num1+num2);
+
+
+ 
+// }
+// // sum(2,4); // 6
+// // sum(2,"4")  //24
+// // sum(2,"a")  //2a
+
+//   const result = sum(2,5);
+
+// console.log(result);  // undefined  // no return 
+
+function sum(num1, num2){
+  return(num1+num2)
+}
+const result = sum(3,7); 
+console.log(result);
