1a20a333c1363ff6c7f3134dc30bcee45dacf248
Rohit173-byte <chamolirohit42@gmail.com>
2024-08-06 18:30:09 +0000

    arrow

---
+++ basic3/arrow.js
@@ -0,0 +1,64 @@
+// // const user = {
+// //  username :"rohit",
+// //  price : 999,
+// //    welcome:function(){
+// //     console.log(`welcome ${this.username} to course`);
+// //    console.log(this);
+    
+// //    }
+   
+   
+   
+// // }
+// // user.welcome()
+// // user.username = "ankit" ;
+// // user.welcome()
+// // console.log(this);
+
+// function chai(){
+// const a =45;
+// console.log(a);
+
+//     console.log(this);
+    
+// }
+// chai()
+
+//  const chai1 = function chai(){
+//     const a =45;
+//     console.log(a);
+    
+//         console.log(this);
+//console.log(this.username);
+        
+//     }
+//     chai()
+
+
+
+const chai = () => {
+    const a =45
+    
+    
+        console.log(this);
+       // console.log(this.a);
+        
+        
+    }
+  // chai()
+
+//   const addnumber = (num1,num2) => {
+//     return num1+num2 
+//   }
+//   console.log(addnumber(6,7));
+  
+    
+
+// const addnumber = (num1,num2) => num1+num2 
+  
+
+// const addnumber = (num1,num2) => (num1+num2)
+
+const addnumber = (num1,num2) => ({username:"rohit"})
+  console.log(addnumber());
+  
