97c1fbd572971006a6eb098df861dad213323444
Rohit173-byte <chamolirohit42@gmail.com>
2024-08-06 07:57:40 +0000

    function

--- basic3/function.js
+++ basic3/function.js
@@ -1,19 +1,35 @@
-// function sum(num1, num2){
-// console.log(num1+num2);
+// // function sum(num1, num2){
+// // console.log(num1+num2);
 
 
  
+// // }
+// // // sum(2,4); // 6
+// // // sum(2,"4")  //24
+// // // sum(2,"a")  //2a
+
+// //   const result = sum(2,5);
+
+// // console.log(result);  // undefined  // no return 
+
+// function sum(num1, num2){
+//   return(num1+num2)
 // }
-// // sum(2,4); // 6
-// // sum(2,"4")  //24
-// // sum(2,"a")  //2a
+// const result = sum(3,7); 
+// console.log(result);
 
-//   const result = sum(2,5);
 
-// console.log(result);  // undefined  // no return 
+function userloggedIN(username){
+ if(username===undefined||null)
+ {console.log("enter your name");
+ 
+  return } 
+   else{
+    return(`${username} just loggedIn` ) 
+   }
+  
 
-function sum(num1, num2){
-  return(num1+num2)
 }
-const result = sum(3,7); 
-console.log(result);
+console.log(userloggedIN("rohit"));
+console.log(userloggedIN(""));    
+console.log(userloggedIN());   // undefined
---
+++ basic3/function_shopping.js
@@ -0,0 +1,35 @@
+// // function shooping(...num1){
+// //  console.log(num1);
+// // return
+// // }
+
+// // shooping(200) ;
+// // shooping(20,60,80,90)
+
+// const obj = {
+//     username:"rohit",
+//     college : "gehu",
+// }
+
+// function handleobject(anyobject) {
+//  console.log(` username is ${anyobject.username} and college is ${anyobject.college }` );
+//  return ;
+// }
+// // method 1
+// // handleobject(obj) ;  
+
+// // method 2
+// handleobject({
+//     username:"rohit",
+//     college:"gehu"
+// })
+
+const myarray = [200,500,700] ;
+function handlearray(array){
+    return(array[2])
+}
+
+ //console.log(handlearray(myarray));
+console.log(handlearray([
+    200,500,900
+]));
