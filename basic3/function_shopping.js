0051e3cab458e3aa362fb6457e831cc1c13f2881
Rohit173-byte <chamolirohit42@gmail.com>
2024-08-07 07:20:43 +0000

    function_shopping

--- basic3/function_shopping.js
+++ basic3/function_shopping.js
@@ -1,28 +1,28 @@
-// // function shooping(...num1){
-// //  console.log(num1);
-// // return
-// // }
+function shooping(...num1){
+ console.log(num1);
+return
+}
 
-// // shooping(200) ;
-// // shooping(20,60,80,90)
+shooping(200) ;
+shooping(20,60,80,90)
 
-// const obj = {
-//     username:"rohit",
-//     college : "gehu",
-// }
+const obj = {
+    username:"rohit",
+    college : "gehu",
+}
 
-// function handleobject(anyobject) {
-//  console.log(` username is ${anyobject.username} and college is ${anyobject.college }` );
-//  return ;
-// }
-// // method 1
-// // handleobject(obj) ;  
+function handleobject(anyobject) {
+ console.log(` username is ${anyobject.username} and college is ${anyobject.college }` );
+ return ;
+}
+// method 1
+// handleobject(obj) ;  
 
-// // method 2
-// handleobject({
-//     username:"rohit",
-//     college:"gehu"
-// })
+// method 2
+handleobject({
+    username:"rohit",
+    college:"gehu"
+})
 
 const myarray = [200,500,700] ;
 function handlearray(array){
