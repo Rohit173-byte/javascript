75689fd09e33a499af8322aff1280bcae8bdd119
Rohit173-byte <chamolirohit42@gmail.com>
2024-08-11 06:20:52 +0000

    for

---
+++ iterations/for.js
@@ -0,0 +1,36 @@
+for (let i = 0; i <=10; i++) {
+    const element =i
+    //console.log(element);
+    
+    
+}
+  for(i=0 ; i <=10 ; i++){
+    const element1 = i;
+    //console.log(`table of ${i}`);
+    
+    for(j=0 ; j<= 10 ; j++){
+        //console.log(`${i}*${j}=${i*j}`);
+        
+    }
+}
+
+for (let index = 1; index<=20; index++) {
+    if(index==5){
+        console.log(`detected 5`);
+        break
+        
+    }
+    console.log(`the value of i is ${index}`);
+    
+    
+}
+for (let index = 1; index<=20; index++) {
+    if(index==5){
+        console.log(`detected 5`);
+        continue
+        
+    }
+    console.log(`the value of i is ${index}`);
+    
+    
+}
